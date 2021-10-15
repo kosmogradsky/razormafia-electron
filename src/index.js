30a59c081576aba1a283c21e555d5e0391d41b74
Evgenii Kosmogradskii-Ivanov <kosmogradsky@gmail.com>
2021-10-15 04:39:06 +0200

    login and connection status

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1600,
     height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   });
 
   win.loadFile(path.resolve(__dirname, "index.html"));
--- src/renderer.js
+++ src/renderer.js
@@ -1,32 +1,109 @@
-import {
-  h,
-  init,
-  classModule,
-  propsModule,
-  styleModule,
-  eventListenersModule,
-} from "./thirdparty/snabbdom.js";
+const net = require("net");
+const socket = net.Socket();
 
-const patch = init([
-  // Init patch function with chosen modules
-  classModule, // makes it easy to toggle classes
-  propsModule, // for setting properties on DOM elements
-  styleModule, // handles styling on elements with support for animations
-  eventListenersModule, // attaches event listeners
-]);
+const connectionStatusSubject = new rxjs.BehaviorSubject("pending");
+
+socket.connect(8080, "127.0.0.1");
+
+socket.on("connect", () => {
+  connectionStatusSubject.next("connected");
+});
+
+socket.on("error", () => {
+  connectionStatusSubject.next("error");
+});
+
+socket.on("data", (data) => {
+  console.log("received data: " + data);
+});
 
 const appDiv = document.createElement("div");
 
-const oldSpanVnode = h("span", { style: { fontWeight: "bold" } }, "This is bold");
-const newSpanVnode = h("span", { style: { fontWeight: "400" } }, "This is italic");
+function ConnectionStatus({ connectionStatus$ }) {
+  const [connectionStatus, setConnectionStatus] = React.useState("pending");
 
-const vnode = h("div#container.two.classes", { on: { click: () => { console.log('onClick') } } }, [
-  oldSpanVnode
-]);
+  React.useEffect(() => {
+    const connectionStatusSubscription =
+      connectionStatus$.subscribe(setConnectionStatus);
+
+    return () => {
+      connectionStatusSubscription.unsubscribe();
+    };
+  }, []);
+
+  function getStatusMessage() {
+    switch (connectionStatus) {
+      case "pending":
+        return "Устанавливаем соединение с сервером...";
+      case "connected":
+        return "Соединение с сервером установлено.";
+      case "error":
+        return "Ошибка соединения с сервером!";
+    }
+  }
+
+  return React.createElement(
+    "div",
+    { style: { marginBottom: "1.25rem" } },
+    getStatusMessage()
+  );
+}
+
+function Login({ socket }) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSetUsername = React.useCallback((event) => {
+    setUsername(event.target.value);
+  }, []);
+
+  const handleSetPassword = React.useCallback((event) => {
+    setPassword(event.target.value);
+  }, []);
+
+  const handleSubmit = React.useCallback((event) => {
+    event.preventDefault();
+
+    socket.write(JSON.stringify({ type: "login", username, password }));
+  }, []);
+
+  return React.createElement(
+    "form",
+    { action: "", onSubmit: handleSubmit },
+    React.createElement(
+      "label",
+      { style: { display: "block", marginBottom: ".5rem" } },
+      React.createElement("span", null, "Имя пользователя: "),
+      React.createElement("input", {
+        type: "text",
+        onChange: handleSetUsername,
+      })
+    ),
+    React.createElement(
+      "label",
+      { style: { display: "block", marginBottom: ".5rem" } },
+      React.createElement("span", null, "Пароль: "),
+      React.createElement("input", {
+        type: "password",
+        onChange: handleSetPassword,
+      })
+    ),
+    React.createElement("button", { type: "submit" }, "Залогиниться")
+  );
+}
+
+function Main({ connectionStatus$, socket }) {
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(ConnectionStatus, { connectionStatus$ }),
+    React.createElement(Login, { socket })
+  );
+}
 
 document.body.appendChild(appDiv);
 
-// Patch into empty DOM element – this modifies the DOM as a side effect
-patch(appDiv, vnode);
-
-patch(oldSpanVnode, newSpanVnode);
+ReactDOM.render(
+  Main({ connectionStatus$: connectionStatusSubject.asObservable(), socket }),
+  appDiv
+);
